a2650137c5faf2ec99c91d40510451d90e92f909
Daru <mica.tui@proton.me>
2024-09-21 13:36:44 +0200

    src/database: refactor database connection

---
+++ conDB.dfm
@@ -0,0 +1,28 @@
+object dmData: TdmData
+  Height = 480
+  Width = 640
+  object dbConnect: TADOConnection
+    Left = 304
+    Top = 216
+  end
+  object tblMeals: TADOTable
+    Left = 416
+    Top = 361
+  end
+  object tblFoods: TADOTable
+    Left = 512
+    Top = 289
+  end
+  object tblUsers: TADOTable
+    Left = 488
+    Top = 185
+  end
+  object dscFoods: TDataSource
+    Left = 168
+    Top = 192
+  end
+  object dscUsers: TDataSource
+    Left = 184
+    Top = 296
+  end
+end
---
+++ conDB.pas
@@ -0,0 +1,80 @@
+unit conDB;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB,Windows,Utils_U;
+
+type
+  TdmData = class(TDataModule)
+    dbConnect: TADOConnection;
+    tblMeals: TADOTable;
+    tblFoods: TADOTable;
+    tblUsers: TADOTable;
+    dscFoods: TDataSource;
+    dscUsers: TDataSource;
+
+    procedure BackUpDB;
+  private
+    { Private declarations }
+    procedure ConnectDB;
+  public
+    { Public declarations }
+  end;
+
+var
+  dmData: TdmData;
+  logger : TLogs;
+
+implementation
+
+procedure TdmData.ConnectDB;
+begin
+  dbConnect := TADOConnection.Create(self);
+  dbConnect.Connected := False;
+  dbConnect.LoginPrompt := False;
+
+  dbConnect.ConnectionString := 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dbBites.mdb;Mode=ReadWrite;Persist Security Info=False;Jet OLEDB:New Database Password="";';
+
+  dbConnect.Provider := 'Provider=Microsoft.Jet.OLEDB.4.0;';
+  dbConnect.Open;
+
+  tblUsers := TADOTable.Create(self);
+  tblUsers.TableName := 'tblUsers';
+  tblUsers.Connection := dbConnect;
+  dscUsers.DataSet := tblUsers;
+
+  tblMeals := TADOTable.Create(self);
+  tblMeals.TableName := 'tblMeals';
+  tblMeals.Connection := dbConnect;
+
+  tblFoods := TADOTable.Create(self);
+  tblFoods.TableName := 'tblFoods';
+  tblFoods.Connection := dbConnect;
+  dscFoods.DataSet := tblFoods;
+
+  tblFoods.Active := True;
+  tblMeals.Active := true;
+  tblUsers.Active := true;
+end;
+
+procedure TdmData.BackUpDB;
+const FILENAME = 'dbBites.mdb';
+var
+  isFailed : Boolean;
+
+begin
+  logger := TLogs.Create;
+  DeleteFile(FILENAME + '.backup');
+  CopyFile(FILENAME,FILENAME+'.backup',isFailed);
+  if isFailed then
+    logger.WriteErrorLog('Error backing up the database')
+	else
+		logger.WriteErrorLog('Database backup successful!');
+end;
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.

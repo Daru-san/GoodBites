2967b0dc5dc1f0e97172606567adedf7038bbd9f
Daru <mica.tui@proton.me>
2024-09-23 20:20:23 +0200

    src/sources/proj: update project sources

--- GoodBites.dpr
+++ GoodBites.dpr
@@ -11,20 +11,19 @@ uses
   Vcl.Styles,
   {$ELSE}
   {$ENDIF }
-  Main_U in 'Main_U.pas' {frmApp},
-  Login_u in 'Login_u.pas' {frmLogin},
-  Dashboard_U in 'Dashboard_U.pas' {frmDashboard},
-  Admin_U in 'Admin_U.pas' {frmAdmin},
-  InfoBoard in 'InfoBoard.pas' {frmInfo},
-  User_U in 'User_U.pas',
-  Utils_U in 'Utils_U.pas',
-  HelpForm in 'HelpForm.pas' {frmHelp},
-  Meals_U in 'Meals_U.pas',
   frmGreeter_U in 'frmGreeter_U.pas' {frmGreeter},
-  frmDataRequest in 'frmDataRequest.pas' {frmFetcher},
   conDB in 'conDB.pas' {dmData: TDataModule},
   OKCANCL2 in 'c:\program files (x86)\embarcadero\studio\23.0\ObjRepos\EN\DelphiWin32\OKCANCL2.PAS' {OKRightDlg},
-  frmAddFood_U in 'frmAddFood_U.pas' {frmAddFood};
+  frmAddFood_U in 'frmAddFood_U.pas' {frmAddFood},
+  frmAdmin_U in 'frmAdmin_U.pas' {frmAdmin},
+  frmDashboard_U in 'frmDashboard_U.pas' {frmDashboard},
+  frmHelp_U in 'frmHelp_U.pas' {frmHelp},
+  frmApp_U in 'frmApp_U.pas' {frmApp},
+  frmLogin_U in 'frmLogin_U.pas' {frmLogin},
+  libUtils_U in 'libUtils_U.pas',
+  libUser_U in 'libUser_U.pas',
+  libMeals_U in 'libMeals_U.pas',
+  libFetchAPI_U in 'libFetchAPI_U.pas';
 
 {$R *.res}
 
@@ -33,15 +32,13 @@ begin
   Application.MainFormOnTaskbar := True;
   TStyleManager.TrySetStyle('Material Oxford Blue SE');
   Application.CreateForm(TfrmApp, frmApp);
-  Application.CreateForm(TfrmLogin, frmLogin);
-  Application.CreateForm(TfrmDashboard, frmDashboard);
-  Application.CreateForm(TfrmAdmin, frmAdmin);
-  Application.CreateForm(TfrmInfo, frmInfo);
-  Application.CreateForm(TfrmHelp, frmHelp);
   Application.CreateForm(TfrmGreeter, frmGreeter);
-  Application.CreateForm(TfrmFetcher, frmFetcher);
   Application.CreateForm(TdmData, dmData);
   Application.CreateForm(TfrmAddFood, frmAddFood);
+  Application.CreateForm(TfrmAdmin, frmAdmin);
+  Application.CreateForm(TfrmDashboard, frmDashboard);
+  Application.CreateForm(TfrmHelp, frmHelp);
+  Application.CreateForm(TfrmLogin, frmLogin);
   Application.Run;
 
 end.

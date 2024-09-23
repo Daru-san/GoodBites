c39c445f9e0344e7a0c7912fe8be1ed5b9c83c1e
Daru <mica.tui@proton.me>
2024-09-23 18:33:05 +0200

    src/db/connection: check if db exists before connecting

--- conDB.pas
+++ conDB.pas
@@ -26,11 +26,13 @@ type
   public
     { Public declarations }
   end;
-
+// DL refers to the name of the database file when in use
+const DBFILENAME = 'dbBites.mdb'; DLFILENAME = 'dbBites.ldb';
 var
   dmData: TdmData;
+  dbPath,dlPath : String;
   logger : TLogs;
-  Util : TUtils;
+  Utils : TUtils;
 
 implementation
 
@@ -40,7 +42,7 @@ begin
   dbConnect.Connected := False;
   dbConnect.LoginPrompt := False;
 
-  dbConnect.ConnectionString := 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\..\dbBites.mdb;Mode=ReadWrite;Persist Security Info=False;Jet OLEDB:New Database Password="";';
+  dbConnect.ConnectionString := 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source='+dbPath+';Mode=ReadWrite;Persist Security Info=False;Jet OLEDB:New Database Password="";';
 
   dbConnect.Provider := 'Provider=Microsoft.Jet.OLEDB.4.0;';
   dbConnect.Open;
@@ -62,31 +64,24 @@ begin
   tblFoods.Active := True;
   tblMeals.Active := true;
   tblUsers.Active := true;
-
-  {
-    Nice to keep the database backup up often, every 5 minutes, unless the `.ldb` file exists, which would mean the database is still in use
-  }
-  timeBackup.Enabled := true;
-  timeBackup.Interval := 5*60;
 end;
 
 procedure TdmData.BackUpDB;
-const FILENAME = 'dbBites.mdb';
 var
   isFailed : Boolean;
-
 begin
 
   { Only backup if the `database.ldb` file is NOT present,
     issues can come up when the database is backed up while still in use,
     I would rather back it up frequently but not when it is in use }
-  if not Util.CheckFileExists('dbBites.ldb') then
-  DeleteFile(FILENAME + '.backup');
-  CopyFile(FILENAME,FILENAME+'.backup',isFailed);
+  if not Util.CheckFileExists(dlPath) then
+  DeleteFile(dbPath + '.backup');
+  CopyFile(dbPath,dbPath+'.backup',isFailed);
+
   if isFailed then
     logger.WriteErrorLog('Error backing up the database')
-	else
-		logger.WriteErrorLog('Database backup successful!');
+  else
+    logger.WriteErrorLog('Database backup successful!');
 end;
 
 {%CLASSGROUP 'Vcl.Controls.TControl'}
@@ -94,10 +89,44 @@ end;
 {$R *.dfm}
 
 procedure TdmData.DataModuleCreate(Sender: TObject);
+var dbExists : Boolean;
 begin
-  ConnectDB;
-  Util.Create;
+  Utils := TUtils.Create;
   logger := TLogs.Create;
+
+  {
+    A series of checks ensuring that the database file exists
+    and stopping the app in the instance that it does not exist
+  }
+  dbExists := false;
+  if Utils.CheckFileExists(DBFILENAME) then
+  begin
+    dbPath := DBFILENAME;
+    dlPath := DLFILENAME;
+    dbExists := true;
+  end
+  else
+  // Checking whether the database is in a higher directory, when running in debug mode
+  if Utils.CheckFileExists('..\..\'+DBFILENAME) then
+  begin
+    dbPath := '..\..\'+DBFILENAME;
+    dlPath := '..\..\'+DLFILENAME;
+    dbExists := true;
+  end
+  else
+  begin
+    logger.WriteErrorLog('The database file is missing');
+    dbExists := false;
+   end;
+
+  if dbExists then
+  begin
+    ConnectDB;
+
+    // Nice to keep the database backup up often, every 5 minutes, unless the `.ldb` file exists, which would mean the database is still in use
+    timeBackup.Enabled := true;
+    timeBackup.Interval := 5*60;
+  end;
 end;
 
 procedure TdmData.DataModuleDestroy(Sender: TObject);

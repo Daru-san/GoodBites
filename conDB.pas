cb6739759e23eb015708c3b749beeb6028233004
Daru <mica.tui@proton.me>
2024-09-23 17:06:32 +0200

    src/conDB: backup database every 5 minutes

--- conDB.pas
+++ conDB.pas
@@ -3,7 +3,8 @@ unit conDB;
 interface
 
 uses
-  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB,Windows,Utils_U;
+  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB,Windows,libUtils_U,
+  Vcl.ExtCtrls;
 
 type
   TdmData = class(TDataModule)
@@ -13,9 +14,12 @@ type
     tblUsers: TADOTable;
     dscFoods: TDataSource;
     dscUsers: TDataSource;
+    timeBackup: TTimer;
 
     procedure BackUpDB;
     procedure DataModuleCreate(Sender: TObject);
+    procedure timeBackupTimer(Sender: TObject);
+    procedure DataModuleDestroy(Sender: TObject);
   private
     { Private declarations }
     procedure ConnectDB;
@@ -26,6 +30,7 @@ type
 var
   dmData: TdmData;
   logger : TLogs;
+  Util : TUtils;
 
 implementation
 
@@ -35,7 +40,7 @@ begin
   dbConnect.Connected := False;
   dbConnect.LoginPrompt := False;
 
-  dbConnect.ConnectionString := 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dbBites.mdb;Mode=ReadWrite;Persist Security Info=False;Jet OLEDB:New Database Password="";';
+  dbConnect.ConnectionString := 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\..\dbBites.mdb;Mode=ReadWrite;Persist Security Info=False;Jet OLEDB:New Database Password="";';
 
   dbConnect.Provider := 'Provider=Microsoft.Jet.OLEDB.4.0;';
   dbConnect.Open;
@@ -57,6 +62,12 @@ begin
   tblFoods.Active := True;
   tblMeals.Active := true;
   tblUsers.Active := true;
+
+  {
+    Nice to keep the database backup up often, every 5 minutes, unless the `.ldb` file exists, which would mean the database is still in use
+  }
+  timeBackup.Enabled := true;
+  timeBackup.Interval := 5*60;
 end;
 
 procedure TdmData.BackUpDB;
@@ -65,7 +76,11 @@ var
   isFailed : Boolean;
 
 begin
-  logger := TLogs.Create;
+
+  { Only backup if the `database.ldb` file is NOT present,
+    issues can come up when the database is backed up while still in use,
+    I would rather back it up frequently but not when it is in use }
+  if not Util.CheckFileExists('dbBites.ldb') then
   DeleteFile(FILENAME + '.backup');
   CopyFile(FILENAME,FILENAME+'.backup',isFailed);
   if isFailed then
@@ -81,6 +96,19 @@ end;
 procedure TdmData.DataModuleCreate(Sender: TObject);
 begin
   ConnectDB;
+  Util.Create;
+  logger := TLogs.Create;
+end;
+
+procedure TdmData.DataModuleDestroy(Sender: TObject);
+begin
+  Util.Free;
+  logger.Free;
+end;
+
+procedure TdmData.timeBackupTimer(Sender: TObject);
+begin
+  BackUpDB;
 end;
 
 end.

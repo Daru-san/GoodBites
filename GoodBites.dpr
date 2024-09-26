91c65ffdeece5f32a6c3199e495fe02ba8fbe24a
Daru <mica.tui@proton.me>
2024-09-26 19:03:56 +0200

    src/proj: update project files

--- GoodBites.dpr
+++ GoodBites.dpr
@@ -9,9 +9,7 @@ uses
   Vcl.Forms,
   Vcl.Themes,
   Vcl.Styles,
-  {$ELSE}
-  {$ENDIF }
-  frmGreeter_U in 'frmGreeter_U.pas' {frmGreeter},
+  {$ENDIF}
   conDB in 'conDB.pas' {dmData: TDataModule},
   OKCANCL2 in 'c:\program files (x86)\embarcadero\studio\23.0\ObjRepos\EN\DelphiWin32\OKCANCL2.PAS' {OKRightDlg},
   frmAddFood_U in 'frmAddFood_U.pas' {frmAddFood},
@@ -23,22 +21,26 @@ uses
   libUtils_U in 'libUtils_U.pas',
   libUser_U in 'libUser_U.pas',
   libMeals_U in 'libMeals_U.pas',
-  libFetchAPI_U in 'libFetchAPI_U.pas';
+  libFetchAPI_U in 'libFetchAPI_U.pas',
+  frmSettings_U in 'frmSettings_U.pas' {frmSettings},
+  frmGreeter_U in 'frmGreeter_U.pas' {frmGreeter},
+  libGoals_U in 'libGoals_U.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  TStyleManager.TrySetStyle('Material Oxford Blue SE');
+  TStyleManager.TrySetStyle('Material Oxford Blue');
   Application.CreateForm(TfrmApp, frmApp);
-  Application.CreateForm(TfrmGreeter, frmGreeter);
   Application.CreateForm(TdmData, dmData);
   Application.CreateForm(TfrmAddFood, frmAddFood);
   Application.CreateForm(TfrmAdmin, frmAdmin);
   Application.CreateForm(TfrmDashboard, frmDashboard);
   Application.CreateForm(TfrmHelp, frmHelp);
   Application.CreateForm(TfrmLogin, frmLogin);
+  Application.CreateForm(TfrmSettings, frmSettings);
+  Application.CreateForm(TfrmGreeter, frmGreeter);
   Application.Run;
 
 end.

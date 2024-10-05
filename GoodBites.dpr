6c0f80b015b9405ed69c192843d8a893bdcc13d8
Daru <mica.tui@proton.me>
2024-10-05 23:16:46 +0200

    src/proj: update source files

--- GoodBites.dpr
+++ GoodBites.dpr
@@ -14,36 +14,34 @@ uses
   frmAddFood_U in 'frmAddFood_U.pas' {frmAddFood},
   frmAdmin_U in 'frmAdmin_U.pas' {frmAdmin},
   frmDashboard_U in 'frmDashboard_U.pas' {frmDashboard},
-  frmHelp_U in 'frmHelp_U.pas' {frmHelp},
   frmApp_U in 'frmApp_U.pas' {frmApp},
   frmLogin_U in 'frmLogin_U.pas' {frmLogin},
   libUtils_U in 'libUtils_U.pas',
   libUser_U in 'libUser_U.pas',
   libMeals_U in 'libMeals_U.pas',
   libFetchAPI_U in 'libFetchAPI_U.pas',
-  frmSettings_U in 'frmSettings_U.pas' {frmSettings},
   libGoals_U in 'libGoals_U.pas',
-  frmGreeter_U in 'frmGreeter_U.pas' {frmGreeter},
+  frmWelcome_U in 'frmWelcome_U.pas' {frmWelcome},
   OKCANCL2 in 'Inherited\OKCANCL2.PAS' {OKRightDlg},
-  frmCustomFood_U in 'frmCustomFood_U.pas' {frmCustomFood};
+  frmCustomFood_U in 'frmCustomFood_U.pas' {frmCustomFood},
+  frmSettings_U in 'frmSettings_U.pas' {frmSettings};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  TStyleManager.TrySetStyle('Material Oxford Blue');
+  TStyleManager.TrySetStyle('Onyx Blue');
   Application.CreateForm(TfrmApp, frmApp);
   Application.CreateForm(TdmData, dmData);
   Application.CreateForm(TfrmAddFood, frmAddFood);
   Application.CreateForm(TfrmAdmin, frmAdmin);
   Application.CreateForm(TfrmDashboard, frmDashboard);
-  Application.CreateForm(TfrmHelp, frmHelp);
   Application.CreateForm(TfrmLogin, frmLogin);
-  Application.CreateForm(TfrmSettings, frmSettings);
-  Application.CreateForm(TfrmGreeter, frmGreeter);
+  Application.CreateForm(TfrmWelcome, frmWelcome);
   Application.CreateForm(TOKRightDlg, OKRightDlg);
   Application.CreateForm(TfrmCustomFood, frmCustomFood);
+  Application.CreateForm(TfrmSettings, frmSettings);
   Application.Run;
 
 end.

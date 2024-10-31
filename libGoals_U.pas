eb54142832a19e648168ef29ffa12350e03bbf68
Daru <mica.tui@proton.me>
2024-11-01 00:16:34 +0200

    src/lib/goals: document

--- libGoals_U.pas
+++ libGoals_U.pas
@@ -27,8 +27,7 @@ type
       property StartDate : TDate read FStartDate write FStartDate;
       property Target : Real read FTarget write FTarget;
 
-      constructor Create(pUserID,pItem : String);
-      destructor Destroy; override;
+			constructor Create(pUserID,pItem : String);
 
       function GetDesc : String;
       // Progress
@@ -58,30 +57,30 @@ begin
   GetGoalDate;
 end;
 
-destructor TGoal.Destroy;
-begin
-
-end;
 { Goals }
 
 {$REGION Goals }
+// Save our goal progress for a particular day
 procedure TGoal.SaveProgress(pAmount: Real);
 var
-  dDate : TDate;
-  iGoalID : Integer;
-  isFound : Boolean;
+	dDate : TDate;
+	iGoalID : Integer;
+	isFound : Boolean;
 begin
-  isFound := False;
+	isFound := False;
 
-  with dmData.tblGoals do
-  begin
-    Open;
-    First;
-    repeat
-      if (GoalID = FieldValues['GoalID']) then
-      begin
-        isFound := true;
-        iGoalID := FieldValues['GoalID'];
+	with dmData.tblGoals do
+	begin
+		Open;
+		First;
+		repeat
+			if (GoalID = FieldValues['GoalID']) then
+			begin
+				isFound := true;
+
+				// We get our goal ID from the database table tblGoals
+				iGoalID := FieldValues['GoalID'];
+        // Log our progress
         LogProgress(iGoalID,pAmount);
       end else Next;
     until EOF or isFound;
@@ -93,21 +92,24 @@ procedure TGoal.LogProgress;
 var
   isFound : Boolean;
 begin
-  isFound := false;
-  with dmData.tblProgress do
-  begin
-    Open;
-    First;
-    repeat
-      if (pGoalID = FieldValues['GoalID']) and (Date = FieldValues['DateRecorded']) then
-      begin
-        isFound := true;
-        Edit;
-        FieldValues['Amount'] := FieldValues['Amount'] + pAmount;
-        Post;
-      end else Next;
-    until EOF or isFound;
+	isFound := false;
+	// If the progress has been saved for the day we can just
+	// edit it and update it
+	with dmData.tblProgress do
+	begin
+		Open;
+		First;
+		repeat
+			if (pGoalID = FieldValues['GoalID']) and (Date = FieldValues['DateRecorded']) then
+			begin
+				isFound := true;
+				Edit;
+				FieldValues['Amount'] := FieldValues['Amount'] + pAmount;
+				Post;
+			end else Next;
+		until EOF or isFound;
 
+    // If not we just save a new item
     if not isFound then
     begin
       Append;
@@ -120,6 +122,7 @@ begin
   end;
 end;
 
+// Reset our goal progress by deleting each item in the table
 procedure TGoal.ResetProgress;
 begin
   with dmData.tblProgress do
@@ -135,9 +138,12 @@ begin
   end;
 end;
 
+// Add our newly created goal to the database
 procedure TGoal.AddGoal;
 begin
-  DeleteGoal;
+	// Delete it in the event that it may somehow exist
+	DeleteGoal;
+
   with dmData.tblGoals do
   begin
     Open;
@@ -153,6 +159,9 @@ begin
   end;
 end;
 
+// Completely remove a goal from the database
+// This requires removing it from both tables
+// Hence we call ResetProgress()
 procedure TGoal.DeleteGoal;
 var isFound : Boolean;
 begin
@@ -174,21 +183,32 @@ begin
   end;
 end;
 
+// Set our new goal target and reset progress
 procedure TGoal.SetGoalTarget;
 var isFound : Boolean;
 begin
-  with dmData.tblGoals do
-  begin
-    Open;
-    First;
-    repeat
-      if (UserID = FieldValues['UserID']) and (GoalID = FieldValues['GoalID']) then
-      begin
-        isFound := true;
-        ResetProgress;
-        Edit;
-        FieldValues['Target'] := Target;
-        FieldValues['StartDate'] := Date;
+	isFound := false;
+	// We find our goal and change the target
+	with dmData.tblGoals do
+	begin
+		Open;
+		First;
+		repeat
+			if (UserID = FieldValues['UserID']) and (GoalID = FieldValues['GoalID']) then
+			begin
+				isFound := true;
+
+				// Restting progress lets us use valid data when making calculations
+				// Instead of using outdated values which would create massive inaccuraceis
+				ResetProgress;
+
+				Edit;
+				FieldValues['Target'] := Target;
+
+				// Restart on the current data
+				FieldValues['StartDate'] := Date;
+
+
         Post;
       end else next;
     until EOF or isFound;
@@ -196,6 +216,9 @@ begin
   end;
 end;
 
+// Get our goal target
+// Used when the object is created to get
+// the values of our specific goal
 procedure TGoal.GetGoalTarget;
 var
   isFound : Boolean;
@@ -219,6 +242,10 @@ begin
   Target := rTarget;
 end;
 
+// Get our goal ID from the database
+// The goal ID is sadly only a simple indexed number
+// in the database table, I could not put the generation
+// together in time
 procedure TGoal.GetGoalID;
 var
   isFound : Boolean;
@@ -238,10 +265,14 @@ begin
       end else Next;
     until EOF or isFound;
     Close;
-  end;
+	end;
+
+	// Our goal ID is set as the property and not a return value
   GoalID := iGoalID;
 end;
 
+
+// Get the start date of our goal
 procedure TGoal.GetGoalDate;
 var isFound : Boolean;
 begin
@@ -262,7 +293,9 @@ begin
   end;
 end;
 
-function TGoal.GetProgress;
+// Get our progress for a particular goal
+// on a specific day
+function TGoal.GetProgress(pRecDate: TDate): Real;
 var
   iGoalID : Integer;
   isFound : Boolean;
@@ -287,6 +320,7 @@ begin
   Result := rValue;
 end;
 
+// Obtain goal description
 function TGoal.GetDesc: string;
 var
   sDesc : String;
@@ -310,6 +344,7 @@ begin
   Result := sDesc;
 end;
 
+// Set our new goal description
 procedure TGoal.EditDesc(pDesc: string);
 var isFound : Boolean;
 begin
@@ -336,16 +371,22 @@ end;
 {$REGION Calculations}
 function TGoal.GetTotalDays: Integer;
 begin
+	// This calculation will always return a real number
+	// so we round, although it may not be completely accurate
   Result := Round(Date - StartDate);
 end;
 
+// Calculate our avearage goal progress
 function TGoal.CalcAverage: Real;
 var
   rTotalVal : real;
   iNumDays : Integer;
 begin
-  iNumDays := GetTotalDays;
-  rTotalVal := 0;
+	// Get our total days since setting our goal
+	iNumDays := GetTotalDays;
+	rTotalVal := 0;
+
+
   with dmData.tblProgress do
   begin
     Open;
@@ -367,6 +408,7 @@ begin
     Result := rTotalVal/iNumDays;
 end;
 
+// Calculate our total achieved days for a goal
 function TGoal.CalcDaysAchieved: Integer;
 var
   rAmount : Real;
